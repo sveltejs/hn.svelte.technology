c99509c9fd9fd6fb5661461372b1123f59601d9d
Rich Harris <richard.a.harris@gmail.com>
2017-12-15 14:43:47 -0500

    update service worker

--- templates/service-worker.js
+++ templates/service-worker.js
@@ -1,10 +1,14 @@
-const CACHE_NAME = `cache__timestamp__`;
-const to_cache = __assets__.concat(__javascript__);
+const ASSETS = `cache__timestamp__`;
+
+const to_cache = __shell__.concat(__assets__);
+const cached = new Set(to_cache);
+
+const routes = __routes__;
 
 self.addEventListener('install', event => {
 	event.waitUntil(
 		caches
-			.open(CACHE_NAME)
+			.open(ASSETS)
 			.then(cache => cache.addAll(to_cache))
 			.then(() => {
 				self.skipWaiting();
@@ -16,7 +20,7 @@ self.addEventListener('activate', event => {
 	event.waitUntil(
 		caches.keys().then(async keys => {
 			for (const key of keys) {
-				if (key !== CACHE_NAME) await caches.delete(key);
+				if (key !== ASSETS) await caches.delete(key);
 			}
 
 			await self.clients.claim();
@@ -25,14 +29,37 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-	if (!/^https?/.test(event.request.url)) return;
+	const url = new URL(event.request.url);
+	if (!/^https?/.test(url.protocol)) return;
 
+	// always serve assets and webpack-generated files from cache
+	if (cached.has(url.pathname)) {
+		event.respondWith(caches.match(event.request));
+		return;
+	}
+
+	// for pages, serve a shell index.html
+	if (url.origin === self.origin && routes.find(route => route.pattern.test(url.pathname))) {
+		event.respondWith(caches.match('/index.html'));
+		return;
+	}
+
+	// for everything else, try the network first, falling back to
+	// cache if the user is offline
 	event.respondWith(
 		caches
-			.open(CACHE_NAME)
+			.open('offline')
 			.then(async cache => {
-				const response = await cache.match(event.request);
-				return response || fetch(event.request);
+				try {
+					const response = await fetch(event.request);
+					cache.put(event.request, response.clone());
+					return response;
+				} catch(err) {
+					const response = await cache.match(event.request);
+					if (response) return response;
+
+					throw err;
+				}
 			})
 	);
 });
