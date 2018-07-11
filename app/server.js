a21beb4ca830d71efea2ef64990986329b8a6ffe
Rich Harris <richard.a.harris@gmail.com>
2018-07-11 17:06:33 -0400

    update to Sapper 0.14

--- app/server.js
+++ app/server.js
@@ -2,9 +2,10 @@ import express from 'express';
 import compression from 'compression';
 import sapper from 'sapper';
 import serve from 'serve-static';
+import App from './App.html';
 import { routes } from './manifest/server.js';
 
 express() // You can also use Express
 	.use(compression({ threshold: 0 }))
-	.use(serve('assets'), sapper({ routes }))
+	.use(serve('assets'), sapper({ App, routes }))
 	.listen(process.env.PORT);
