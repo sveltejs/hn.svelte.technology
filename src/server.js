92febf1d9ede6afbefb32d2dd91dd370ab0bba8c
Rich Harris <richard.a.harris@gmail.com>
2018-09-30 15:31:52 -0400

    update for 0.22

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import compression from 'compression';
-import sapper from 'sapper';
 import serve from 'serve-static';
-import { manifest } from './manifest/server.js';
+import * as sapper from '../__sapper__/server.js';
 
 express() // You can also use Express
-	.use(compression({ threshold: 0 }))
-	.use(serve('static'), sapper({ manifest }))
+	.use(
+		compression({ threshold: 0 }),
+		serve('static'),
+		sapper.middleware()
+	)
 	.listen(process.env.PORT);
